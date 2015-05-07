7fd3d7196a669b99b365c1caf01dd8cbaf4ade26
rudiluis <rudi121_kemo@hotmail.com>
2015-05-07 20:37:32 +0100

    Bugs fixes

--- app/src/main/java/pt/iscte/drones_swarm_mobile_app/Activities/SecondActivity.java
+++ app/src/main/java/pt/iscte/drones_swarm_mobile_app/Activities/SecondActivity.java
@@ -24,9 +24,6 @@ public class SecondActivity extends ActionBarActivity {
 
 
 
-
-
-
     Button buttonStart = (Button) findViewById(R.id.button_start);
 
         buttonStart.setOnClickListener(new View.OnClickListener() {
@@ -40,25 +37,4 @@ public class SecondActivity extends ActionBarActivity {
     }
 
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_second, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
