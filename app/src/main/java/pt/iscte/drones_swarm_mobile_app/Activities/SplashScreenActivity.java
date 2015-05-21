89f73f1780cf8e1e005381f29ae6fc838c1f4bbb
rudiluis <rudi121_kemo@hotmail.com>
2015-05-21 23:20:10 +0100

    Algumas alterações!

--- app/src/main/java/pt/iscte/drones_swarm_mobile_app/Activities/SplashScreenActivity.java
+++ app/src/main/java/pt/iscte/drones_swarm_mobile_app/Activities/SplashScreenActivity.java
@@ -5,6 +5,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.ProgressBar;
 
 import pt.iscte.drones_swarm_mobile_app.R;
 
@@ -25,7 +26,7 @@ public class SplashScreenActivity extends Activity {
             public void run() {
                 try {
                     int waited = 0;
-                    while (waited < 1000) {
+                    while (waited < 4000) {
                         sleep(100);
                         waited += 100;
                     }
