a445d0e2ec25eb31978b99d489ed203d88a133fb
rudiluis <rudi121_kemo@hotmail.com>
2015-05-22 15:30:35 +0100

    About pronto!

--- app/src/main/java/pt/iscte/drones_swarm_mobile_app/Activities/AboutActivity.java
+++ app/src/main/java/pt/iscte/drones_swarm_mobile_app/Activities/AboutActivity.java
@@ -5,6 +5,8 @@ import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.Window;
+import android.view.WindowManager;
 
 import pt.iscte.drones_swarm_mobile_app.R;
 
@@ -13,7 +15,12 @@ public class AboutActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        // ----------------FULLSCREEN WITH LAYOUT(START)-----------------//
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_about);
+        // ----------------FULLSCREEN WITH LAYOUT(END)-----------------//
     }
 
 
