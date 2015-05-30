e5e7dc9e9a45df6eb19b37e669ce92fb1988b59c
Rudi Luis <rudi121_kemo@hotmail.com>
2015-05-30 22:21:26 +0100

    Configure

--- app/src/main/java/pt/iscte/drones_swarm_mobile_app/activity/ConfigureActivity.java
+++ app/src/main/java/pt/iscte/drones_swarm_mobile_app/activity/ConfigureActivity.java
@@ -1,28 +1,13 @@
 package pt.iscte.drones_swarm_mobile_app.activity;
 
 import android.app.Activity;
-import android.app.Dialog;
-import android.content.Intent;
-import android.graphics.drawable.Drawable;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.ImageButton;
-import android.widget.ImageView;
-import android.widget.Spinner;
-import android.widget.TextView;
 import android.widget.Toast;
-
-import java.util.ArrayList;
-import java.util.List;
 
 import pt.iscte.drones_swarm_mobile_app.R;
 
@@ -39,53 +24,25 @@ public class ConfigureActivity extends Activity {
         setContentView(R.layout.activity_configure);
         // ----------------FULLSCREEN WITH LAYOUT(END)-----------------//
 
-        addListenerOnSpinnerItemSelection();
         interactions();
     }
 
-    public void addListenerOnSpinnerItemSelection() {
-        Spinner spinner_configure = (Spinner) findViewById(R.id.spinner_configure);
-
-        List<String> list = new ArrayList<String>();
-        list.add("list 1");
-        list.add("list 2");
-        list.add("list 3");
-        ArrayAdapter<String> dataAdapter = new ArrayAdapter<String>(this,
-                android.R.layout.simple_spinner_item, list);
-        dataAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        spinner_configure.setAdapter(dataAdapter);
-        spinner_configure.setOnItemSelectedListener(new CustomOnItemSelectedListener());
-    }
 
     private void interactions(){
-        ImageButton button1 = (ImageButton) findViewById(R.id.button_1);
+        EditText editTextIP = (EditText) findViewById(R.id.editText_ip_configure_activity);
+        editTextIP.setText( "192.168.2.1");
 
-        button1.setOnClickListener(new View.OnClickListener()
+        Button button_update = (Button) findViewById(R.id.button_update_configure_activity);
+
+        button_update.setOnClickListener(new View.OnClickListener()
 
         {
             @Override
             public void onClick(View v) {
-                Toast.makeText(ConfigureActivity.this, "Entrou botao 1", Toast.LENGTH_SHORT).show();
+                Toast.makeText(ConfigureActivity.this, "Entrou no botao update!", Toast.LENGTH_SHORT).show();
 
             }
         });
-
-        EditText editTextIP = (EditText) findViewById(R.id.textView_ip_configure_activity);
-        editTextIP.setText( "192.168.2.1");
-
-    }
-    public class CustomOnItemSelectedListener implements AdapterView.OnItemSelectedListener {
-
-        public void onItemSelected(AdapterView<?> parent, View view, int pos,long id) {
-            Toast.makeText(parent.getContext(),
-                    "OnItemSelectedListener : " + parent.getItemAtPosition(pos).toString(),
-                    Toast.LENGTH_SHORT).show();
-        }
-
-        @Override
-        public void onNothingSelected(AdapterView<?> arg0) {
-
-        }
 
     }
 
